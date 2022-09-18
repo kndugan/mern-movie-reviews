e3c4d874e60144f339ebaf38a8c9493610b2ec2d
Kristine Dugan <kndugan@gmail.com>
2022-09-17 15:31:35 -1000

    Add Post, Put, and Delete Reviews

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import MoviesController from './movies.controller.js';
+import ReviewsController from './reviews.controller.js'
 
 // get access to Express Router
 const router = express.Router();
 
 router.route('/').get(MoviesController.apiGetMovies)
+
+router  
+    .route("/review")
+    .post(ReviewsController.apiPostReview)
+    .put(ReviewsController.apiUpdateReview)
+    .delete(ReviewsController.apiDeleteReview)
 
 // router.route('/').get((req,res) => res.send('hello world'));
 
